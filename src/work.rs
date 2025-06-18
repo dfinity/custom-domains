e7199760acc16acf832a21b0f9811ebf2d0b177f
Nikolay Komarevskiy <nikolay.komarevskiy@dfinity.org>
2025-06-18 10:44:20 +0200

    chore: add clippy job

--- src/work.rs
+++ src/work.rs
@@ -1,61 +1,18 @@
-use std::{
-    sync::{Arc, Mutex},
-    time::Duration,
-};
+use std::{sync::Arc, time::Duration};
 
-use thiserror::Error;
 use tokio::{select, time::sleep};
 use tokio_util::sync::CancellationToken;
-use tracing::{debug, error, info, warn};
+use tracing::{error, info, warn};
 use trait_async::trait_async;
 
-use crate::task::{Task, TaskOutput, TaskStatus};
+use crate::task::{ManagesTasks, Task, TaskError, TaskOutput, TaskStatus};
 
 const POLLING_INTERVAL_NO_TASKS: Duration = Duration::from_secs(15);
 const TASK_EXECUTION_TIMEOUT: Duration = Duration::from_secs(5);
-const MAX_TASK_FAILURES: u32 = 10;
-
-#[derive(Debug, Error)]
-pub enum TaskError {}
 
 #[trait_async]
 pub trait RunsTasks {
     async fn run(&self);
-}
-
-#[trait_async]
-pub trait ManagesTasks: Send + Sync {
-    async fn fetch(&self) -> Result<Option<Task>, TaskError>;
-    async fn submit(&self, task: TaskOutput) -> Result<(), TaskError>;
-}
-
-pub struct TaskManager {
-    pub tasks: Arc<Mutex<Vec<Task>>>,
-}
-
-#[trait_async]
-impl ManagesTasks for TaskManager {
-    async fn fetch(&self) -> Result<Option<Task>, TaskError> {
-        let task = {
-            let mut lock = self.tasks.lock().unwrap();
-            let task = lock.pop();
-            debug!("fetched task is {task:?}");
-            task
-        };
-
-        sleep(Duration::from_secs(20)).await;
-        Ok(task)
-    }
-
-    async fn submit(&self, task: TaskOutput) -> Result<(), TaskError> {
-        let mut lock = self.tasks.lock().unwrap();
-        if task.task.failures < MAX_TASK_FAILURES {
-            lock.push(task.clone().task);
-        } else {
-            debug!("task {task:?} failed too many times, dropping");
-        }
-        Ok(())
-    }
 }
 
 pub struct Worker {
@@ -93,7 +50,7 @@ impl RunsTasks for Worker {
                                 result = execute(task) => {
                                     match result {
                                         Ok(result) => {
-                                            let _ = task_manager.submit(result).await;
+                                            let _ = task_manager.submit_result(result).await;
                                         }
                                         Err(err) => {
                                             error!("Failed to execute task: {:?}", err);
