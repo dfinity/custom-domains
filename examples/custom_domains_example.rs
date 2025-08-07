bbac7f9d492e1645683fc57f937cc43124b72e75
Nikolay Komarevskiy <nikolay.komarevskiy@dfinity.org>
2025-08-07 20:42:31 +0000

    fix: example

---
+++ examples/custom_domains_example.rs
@@ -0,0 +1,59 @@
+use std::{env, net::SocketAddr, sync::Arc};
+
+use backend::router::create_router;
+use base::types::{
+    acme::AcmeClientConfig,
+    cipher::CertificateCipher,
+    validator::Validator,
+    worker::{Worker, WorkerConfig},
+};
+use canister_client::canister_repository::CanisterClient;
+use ic_agent::Agent;
+use prometheus::Registry;
+use tokio::spawn;
+use tokio_util::sync::CancellationToken;
+use tracing::info;
+
+// $ cargo run --example custom_domains_example
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Initialize tracing
+    tracing_subscriber::fmt::init();
+
+    let canister_id = env::var("CANISTER_ID").expect("CANISTER_ID var is not set");
+    let cloudflare_api_token =
+        env::var("CLOUDFLARE_API_TOKEN").expect("CLOUDFLARE_API_TOKEN var is not set");
+
+    let cipher = Arc::new(CertificateCipher::new());
+    let agent = Agent::builder().with_url("https://ic0.app").build()?;
+    let canister_id = canister_id.parse().expect("Invalid CANISTER_ID format");
+    let repository = Arc::new(CanisterClient::new(agent, canister_id, cipher));
+    let validator = Arc::new(Validator::default());
+
+    info!("starting worker, which peforms all tasks ...");
+    let token = CancellationToken::new();
+    let acme_client = Arc::new(AcmeClientConfig::new(cloudflare_api_token).build().await?);
+    let registry = Registry::new_custom(Some("custom_domains".into()), None).unwrap();
+    let worker = Worker::new(
+        "worker_1".to_string(),
+        repository.clone(),
+        validator.clone(),
+        acme_client,
+        WorkerConfig::default(),
+        registry.clone(),
+        token.clone(),
+    );
+    spawn(async move { worker.run().await });
+
+    let app = create_router(repository.clone(), validator, registry, true);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    info!("Starting server on http://{}", addr);
+    axum_server::bind(addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+    Ok(())
+}
